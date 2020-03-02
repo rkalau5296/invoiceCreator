870a96b473bb10363e0fab1ea1832dcb7186ed85
rkalau <rafal.kalata.itservice@gmail.com>
2020-03-02 15:05:01 +0100

    test problem

--- frontend/src/test/java/com/example/frontend/FrontendApplicationTests.java
+++ frontend/src/test/java/com/example/frontend/FrontendApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FrontendApplicationTests {
+public class FrontendApplicationTests {
 
     @Test
     void contextLoads() {
